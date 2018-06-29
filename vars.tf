93749fac48d46801262914156383bd31042ea3d2
Damodaran R <damodaran_r@outlook.com>
2018-06-29 18:24:18 +0530

    Create vars.tf

---
+++ vars.tf
@@ -0,0 +1,14 @@
+variable "s3-source-buc-name"
+{
+    default = "lambda-source-up-test"
+} 
+
+variable "s3-destination-buc-name"
+{
+    default = "lambda-dest"
+}
+
+variable "lambda_name" 
+{
+    default = "demo_lambda"
+}
